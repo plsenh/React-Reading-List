0b82e9ed4b33614c62cb7c89fab37bc94e751b4d
PLS <plsenh@gmail.com>
2019-02-14 20:53:43 -0500

    20190214

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -31,7 +31,7 @@ class Detail extends Component {
           <Col size="md-10 md-offset-1">
             <article>
               <h1>Synopsis</h1>
-              <p>{this.state.book.synopsis}</p>
+              <p>{this.state.book.synopsis}</p>heroku apps:
             </article>
           </Col>
         </Row>
