2caab086607436d04cc17e1986780cc5ee1870f6
PLS <plsenh@gmail.com>
2019-02-14 21:13:40 -0500

    fixed typo

---
+++ client/build/precache-manifest.cc88cfaf621f84582f269a0370838a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eb13997329f6d616b2a5",
+    "url": "/static/js/main.eb139973.chunk.js"
+  },
+  {
+    "revision": "8d2fef64904637c8ac76",
+    "url": "/static/js/2.8d2fef64.chunk.js"
+  },
+  {
+    "revision": "eb13997329f6d616b2a5",
+    "url": "/static/css/main.3017f8b5.chunk.css"
+  },
+  {
+    "revision": "86ced1697626f1110859cbbb1ad4d89b",
+    "url": "/index.html"
+  }
+];
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -31,7 +31,7 @@ class Detail extends Component {
           <Col size="md-10 md-offset-1">
             <article>
               <h1>Synopsis</h1>
-              <p>{this.state.book.synopsis}</p>heroku apps:
+              <p>{this.state.book.synopsis}</p>
             </article>
           </Col>
         </Row>
